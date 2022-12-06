85e1f18f049b0e1e8b3b1e78c9f79302c2f3a389
karolNiedziela <imniedziel@gmail.com>
2022-12-06 21:07:35 +0100

    Day 6

---
+++ Day_6/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllText("input.txt");
+
+int GetMarkerIndex(string input, int numberofUniqueCharacters)
+{
+    var markerIndex = 0;
+
+    for (var i = 0; i < input.Length - numberofUniqueCharacters; i++)
+    {
+        var substring = input.Substring(i, numberofUniqueCharacters);
+        var uniqueCharacters = substring.Distinct().Count();
+        if (uniqueCharacters == numberofUniqueCharacters)
+        {
+            markerIndex = i + numberofUniqueCharacters;
+            break;
+        }
+    }
+
+    return markerIndex;
+}
+
+//var part1 = GetMarkerIndex(input, 4);
+var part2 = GetMarkerIndex(input, 14);
+
+Console.WriteLine(part2);
+
+Console.ReadKey();
